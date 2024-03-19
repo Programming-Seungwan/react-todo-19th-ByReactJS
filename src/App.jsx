0bd5c32c01c48b5a4fcdd8d8b1f86b4f4c90cdb9
Programming-Seungwan <swsj4480@gmail.com>
2024-03-20 00:57:54 +0900

    Chore : make default total layout

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+// 실질적으로 react application의 진입점이 되는 파일임
+import AppContainer from './components/AppContainer';
+
+function App() {
+  return <AppContainer>
+    
+  </AppContainer>
+}
+
+export default App;
---
+++ src/components/AppContainer.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const StyledAppContainer = styled.main`
+  width: 70%;
+  max-width: 70%;
+  min-width: 70%;
+  height: 80vh;
+`
+
+export default function AppContainer() {
+  return <StyledAppContainer>hi</StyledAppContainer>
+}
